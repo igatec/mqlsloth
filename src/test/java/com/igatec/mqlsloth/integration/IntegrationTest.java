f024fb1fa4b1e85be0e1b2b5305a14f104bc5499
kutioff <stepan.kutyev@gmail.com>
2020-07-31 09:10:45 +0300

    #12: Test verifying file writing

--- src/test/java/com/igatec/mqlsloth/integration/IntegrationTest.java
+++ src/test/java/com/igatec/mqlsloth/integration/IntegrationTest.java
@@ -4,27 +4,36 @@ import com.igatec.mqlsloth.cli.SlothAppCLI;
 import com.igatec.mqlsloth.context.ApplicationContext;
 import com.igatec.mqlsloth.framework.Context;
 import com.igatec.mqlsloth.framework.MQLCommand;
+import com.igatec.mqlsloth.io.fs.FileWriterService;
 import com.igatec.mqlsloth.kernel.SlothException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 class IntegrationTest {
 
     private final MQLCommand command = mock(MQLCommand.class);
+    private final FileWriterService fileWriterService = mock(FileWriterService.class);
 
     @BeforeEach
     void setUp() {
         ApplicationContext.instance().setMqlCommand(command);
+        ApplicationContext.instance().setFileWriterService(fileWriterService);
     }
 
     @Test
@@ -34,6 +43,23 @@ class IntegrationTest {
         mockCommand("list property on program eServiceSchemaVariableMapping.tcl to type Document", "");
 
         SlothAppCLI.main(generateContext(), args("-e -p type Document -t ."));
+
+        verifyFileExport(".\\dataModel\\type\\Document.yml", "dataModel/type/Document.yml");
+    }
+
+    private void verifyFileExport(String expectedFilePath, String expectedContentPath) throws IOException {
+        ArgumentCaptor<File> fileCaptor = ArgumentCaptor.forClass(File.class);
+        ArgumentCaptor<String> contentCaptor = ArgumentCaptor.forClass(String.class);
+        verify(fileWriterService).writeNewFile(fileCaptor.capture(), contentCaptor.capture());
+
+        String actualExportPath = fileCaptor.getValue().getPath();
+        assertThat(actualExportPath, equalTo(expectedFilePath));
+
+        String actualContent = contentCaptor.getValue();
+        assertThat(
+                readFileFromClasspath("/slothoutput/" + expectedContentPath).trim(),
+                equalTo(actualContent.trim())
+        );
     }
 
     private void mockCommand(String cmd, String result) throws SlothException {
@@ -46,11 +72,7 @@ class IntegrationTest {
     private void mockCommandUsingFile(String cmd, String fileWithResult) throws SlothException {
         String[] cmdArr = cmd.split(" ");
 
-        String fileContent = new BufferedReader(new InputStreamReader(
-                getClass().getResourceAsStream("/mqloutput/" + fileWithResult)
-        ))
-                .lines()
-                .collect(Collectors.joining("\n"));
+        String fileContent = readFileFromClasspath("/mqloutput/" + fileWithResult);
 
         when(command.executeOrThrow(any(), eq(generateCommandTemplate(cmdArr)), eq(Arrays.asList(cmdArr))))
                 .thenReturn(fileContent);
@@ -65,8 +87,16 @@ class IntegrationTest {
             sb.append(" ");
             i++;
         }
-        sb.deleteCharAt(sb.length() -1);
+        sb.deleteCharAt(sb.length() - 1);
         return sb.toString();
+    }
+
+    private String readFileFromClasspath(String filePath) {
+        return new BufferedReader(new InputStreamReader(
+                getClass().getResourceAsStream(filePath)
+        ))
+                .lines()
+                .collect(Collectors.joining("\n"));
     }
 
     private static String[] args(String args) {
